c5c555258ab4ae48973c6c35b9fbf65191a258d2
Deepa <52133154+Deepacat@users.noreply.github.com>
2024-10-15 19:25:11 -0500

    disabled portal
    apparently kjs can register train portals fine with this coremod so I am gonna keep it there unless there's some new limitation

--- src/main/java/com/deepacat/WorldshaperCore/WSportals.java
+++ src/main/java/com/deepacat/WorldshaperCore/WSportals.java
@@ -2,17 +2,16 @@ package com.deepacat.WorldshaperCore;
 
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
-import net.kyrptonaught.customportalapi.CustomPortalBlock;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 
 public class WSportals {
 	public static void init() {
-		CustomPortalBuilder.beginPortal()
-				.frameBlock(GTBlocks.COIL_TRINIUM.get())
-				.destDimID(new ResourceLocation("minecraft:the_end"))
-				.lightWithItem(Items.DIAMOND)
-				.tintColor(33, 0, 33)
-				.registerPortal();
+//		CustomPortalBuilder.beginPortal()
+//				.frameBlock(GTBlocks.COIL_TRINIUM.get())
+//				.destDimID(new ResourceLocation("minecraft:the_end"))
+//				.lightWithItem(Items.DIAMOND)
+//				.tintColor(33, 0, 33)
+//				.registerPortal();
 	}
 }
