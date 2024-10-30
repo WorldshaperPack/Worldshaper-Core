c7e311bccb3efe9bc34bbc3ec02d0bb405e3af31
Levlion2 <121055316+Levlion2@users.noreply.github.com>
2024-10-30 11:58:30 +0000

    it somewhat weorks and thats good enough

---
+++ src/main/java/com/deepacat/WorldshaperCore/api/registries/WSRegistries.java
@@ -0,0 +1,11 @@
+package com.deepacat.WorldshaperCore.api.registries;
+
+import com.deepacat.WorldshaperCore.WorldshaperCore;
+import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.api.cover.CoverDefinition;
+import com.gregtechceu.gtceu.api.registry.GTRegistry;
+import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
+
+public class WSRegistries {
+    public static final GTRegistrate REGISTRATE = GTRegistrate.create(WorldshaperCore.MOD_ID);
+}
---
+++ src/main/java/com/deepacat/WorldshaperCore/common/data/WSMachines.java
@@ -0,0 +1,49 @@
+package com.deepacat.WorldshaperCore.common.data;
+
+
+import com.deepacat.WorldshaperCore.common.machine.multiblock.ParallelMultiblock;
+import com.gregtechceu.gtceu.GTCEu;
+import com.gregtechceu.gtceu.api.data.RotationState;
+import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
+import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
+import com.gregtechceu.gtceu.api.pattern.Predicates;
+import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
+import com.gregtechceu.gtceu.common.data.GTCompassSections;
+import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
+import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
+import net.minecraftforge.event.level.PistonEvent;
+
+
+import static com.deepacat.WorldshaperCore.api.registries.WSRegistries.REGISTRATE;
+import static com.gregtechceu.gtceu.api.GTValues.*;
+import static com.gregtechceu.gtceu.api.pattern.Predicates.blocks;
+import static com.gregtechceu.gtceu.common.data.GTBlocks.*;
+
+
+public class WSMachines {
+    public static final MultiblockMachineDefinition twobytwowiremill = REGISTRATE
+            .multiblock("idk", holder -> new ParallelMultiblock(holder, 4))
+            .rotationState(RotationState.ALL)
+            .recipeType(GTRecipeTypes.WIREMILL_RECIPES)
+            .alwaysTryModifyRecipe(true)
+            .recipeModifiers(GTRecipeModifiers.DEFAULT_ENVIRONMENT_REQUIREMENT,
+                    GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK_SUBTICK),
+                    WSRecipeModifiers.SETPARALLEL)
+            .appearanceBlock(CASING_STEEL_SOLID)
+            .pattern(definition -> FactoryBlockPattern.start()
+                    .aisle("FF", "FF")
+                    .aisle("FF", "FS")
+                    .where('S', Predicates.controller(blocks(definition.getBlock())))
+                    .where('F', blocks(CASING_TITANIUM_STABLE.get()).or(Predicates.autoAbilities(definition.getRecipeTypes())))
+                    .build())
+            .workableCasingRenderer(GTCEu.id("block/casings/solid/machine_casing_stable_titanium"),
+                    GTCEu.id("block/multiblock/primitive_blast_furnace"))
+            .compassSections(GTCompassSections.TIER[IV])
+            .compassNodeSelf()
+            .register();
+
+
+    public static void init(){
+
+    }
+}
