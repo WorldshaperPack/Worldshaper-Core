7c4e8dbb2514ec85dc0399d338f87f4a11e8da15
Deepa <52133154+Deepacat@users.noreply.github.com>
2024-11-10 00:13:17 -0600

    portal fix

--- src/main/java/com/deepacat/WorldshaperCore/WSPortals.java
+++ src/main/java/com/deepacat/WorldshaperCore/WSPortals.java
@@ -1,12 +1,18 @@
 package com.deepacat.WorldshaperCore;
 
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
+
 public class WSPortals {
 	public static void init() {
-//		CustomPortalBuilder.beginPortal()
-//				.frameBlock(GTBlocks.COIL_TRINIUM.get())
-//				.destDimID(new ResourceLocation("minecraft:the_end"))
-//				.lightWithItem(Items.DIAMOND)
-//				.tintColor(33, 0, 33)
-//				.registerPortal();
+//		A placeholder portal needs to be registered for the train compat to work with Kubejs so I guess this is here
+		CustomPortalBuilder.beginPortal()
+				.frameBlock(GTBlocks.MACHINE_CASING_UHV.get())
+				.destDimID(new ResourceLocation("minecraft:the_nether"))
+				.lightWithItem(Items.DIAMOND)
+				.tintColor(33, 0, 33)
+				.registerPortal();
 	}
 }
